00b913a7fc05a9a69b088b926eb029d1a3066a0d
akshayabaiju-dev <akshayabaiju20@gmail.com>
2025-06-17 18:28:26 +0530

    api file edit

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CareerController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ServiceController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -16,5 +17,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
     Route::apiResource('careers', CareerController::class);
+     Route::apiResource('services', ServiceController::class);
+      Route::apiResource('contacts', ContactController::class);
     
 });
