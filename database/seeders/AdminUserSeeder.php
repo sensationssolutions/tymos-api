9c44b0bf98e1f94a4a6f1ccc15a06f843d174ce8
Ajith <ajithmanoj1010@gmail.com>
2025-06-17 18:24:31 +0530

    Login api

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password123'),
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CareerController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
+    Route::apiResource('careers', CareerController::class);
+    
 });
-
-
-
-Route::apiResource('careers', CareerController::class);
